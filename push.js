969cfe137024504e05f74e0a4b5126cae7ec6826
Ashfaq Ali Zardari <ashfaqalizardariofficial@gmail.com>
2021-02-25 12:44:50 +0500

    fix

--- push.js
+++ push.js
@@ -5,9 +5,9 @@ let vapidkeys = {
     privateKey: 'lfOOl0SfA4Xqx5sWwRdxEalUzEHykqYHS3zLqbHoG7Y'
 };
 
-push.setVapidDetails('mailto:ashfaqalizardariofficial@gmail.com', vapidkeys.publicKey, vapidkeys.privateKey);
+push.setVapidDetails('mailto:test@code.co.uk', vapidkeys.publicKey, vapidkeys.privateKey);
 
 // let sub = {};
-let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/eOSZ0qslxNg:APA91bHS2yOMb0Y5vEZxq8mb5Qo3w3FNkvgZeF1RiyjFZCVO_NlDJ57fMkAPfERpOyz8jKW2eKxBIwRxoUZC21MOX5tFBbXyLBCpLJHUCXufiADRWtA0CB5ao8BIQtzMyCV96u9lP2V8", "expirationTime": null, "keys": { "p256dh": "BOJwfgxHZJyUQwNwBgtXwdPp-87obxC1b9x02jQYbSliO1rOvNQR8CvP0uA3xUEmmv4M_HTgYPTfub-Kn8mD9TY", "auth": "3O0NCVXR_6E9GunuGMF46g" } };
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/eOSZ0qslxNg:APA91bHS2yOMb0Y5vEZxq8mb5Qo3w3FNkvgZeF1RiyjFZCVO_NlDJ57fMkAPfERpOyz8jKW2eKxBIwRxoUZC21MOX5tFBbXyLBCpLJHUCXufiADRWtA0CB5ao8BIQtzMyCV96u9lP2V8","expirationTime":null,"keys":{"p256dh":"BOJwfgxHZJyUQwNwBgtXwdPp-87obxC1b9x02jQYbSliO1rOvNQR8CvP0uA3xUEmmv4M_HTgYPTfub-Kn8mD9TY","auth":"3O0NCVXR_6E9GunuGMF46g"}};
 
 push.sendNotification(sub, 'Test Message');
